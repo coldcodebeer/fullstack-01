bbfade47107f87840ed47e6a472b8cb145fa2715
Spencer <yuanyuan.zhang@everbridge.com>
2023-10-21 23:38:22 +0800

    CD Workflow

--- backend/src/main/java/com/spencercode/springbootexample/PingPongController.java
+++ backend/src/main/java/com/spencercode/springbootexample/PingPongController.java
@@ -3,6 +3,8 @@ package com.spencercode.springbootexample;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// for testing cd
+
 @RestController
 public class PingPongController {
     record PingPong(String result) {};
